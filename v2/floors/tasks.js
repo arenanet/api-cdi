8ad156a98721b91954beeb5ee15793f29463bdea
Pat Cavit <pcavit@gmail.com>
2015-02-19 10:52:34 -0800

    New format for /v2/floors, moved under continents
    - New queryable segments of a floor

---
+++ v2/floors/tasks.js
@@ -0,0 +1,7 @@
+// GET /v2/continents/1/floors/1/regions/1/maps/26/tasks/7
+{
+    "objective" : "Help Explorer Brokkar sabotage dredge munitions.",
+    "level" : 49,
+    "coord" : [ 20414.9, 17761.3 ],
+    "id" : 7
+}
