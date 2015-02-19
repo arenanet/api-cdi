304a2e0dc637ed19fb3d4d62bb6a2b1d0b85b920
lye <lye@>
2015-02-19 11:52:18 -0800

    Add the bank/materials fields to the /v2/account example.
    This is for illustrative purposes to show how multiple result sets are
    merged onto the same response object, depending on the requester's scopes.

--- v2/account/account.js
+++ v2/account/account.js
@@ -7,3 +7,31 @@
 	"name"  : "Lawton.1234",
 	"world" : 1007
 }
+
+// With the "account" and "inventories" scopes:
+{
+	"id"        : "account-guid",
+	"name"      : "Lawton.1234",
+	"world"     : 1007
+	"bank"      : [
+		{
+			"id"       : 19699,
+			"slot"     : 1,
+			"count"    : 250
+		},
+		{
+			"id"       : 30692,
+			"slot"     : 2,
+			"count"    : 1,
+			"upgrades" : [ 24607 ]
+		}
+	],
+	"materials" : [
+		{
+			"id"       : 19699,
+			"slot"     : 1,
+			"count"    : 250
+		}
+	]
+}
+
