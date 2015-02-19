cc9f45997f89b3e5b551ca216228774f4b688d3d
samuirai <samuirai@samuirai-macbook.fritz.box>
2015-02-19 21:56:35 +0100

    added example character api response

---
+++ v2/characters/character.js
@@ -0,0 +1,18 @@
+// POST /v2/characters/1234abcdefgh-auth-token/1
+// body:
+// { "action": "add_level", "amount": 1 }
+
+// POST /v2/characters/1234abcdefgh-auth-token/1
+// body:
+// { "action": "add_gold", "amount": 100000 }
+
+// POST /v2/characters/1234abcdefgh-auth-token/1
+// body:
+// { "action": "add_karma", "amount": 10000000 }
+
+// POST /v2/characters/1234abcdefgh-auth-token/1
+// body:
+// { "action": "add_item", "item_id": 30689 }
+{
+    "status": "success"
+}
