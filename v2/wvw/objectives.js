50f26fe2042943e447240184347fcb716ce4f0cc
smiley <smiley@chillerlan.net>
2015-02-20 15:20:42 +0100

    proposal for objectives.json

---
+++ v2/wvw/objectives.js
@@ -0,0 +1,22 @@
+{
+  // iterator -> objective_id
+  "1": {
+
+    // score and type are mutually interchangable
+    "score": 25,
+    "type": "keep",
+
+    // absolute coordinates of the marker icon
+    "coords": [10766, 13656],
+
+    // i prefer having all languages with one request
+    // a little more overhead is still faster than x requests
+    // where most of the response is the same anyway
+    "name": {
+      "de": "Aussichtspunkt",
+      "en": "Overlook",
+      "es": "Mirador",
+      "fr": "Belvédère"
+    }
+  }
+}
