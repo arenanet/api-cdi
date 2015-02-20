96a0381b0dcf29af4719e1cbc3dcdca471fc9997
SamHurne <SamHurne@gmail.com>
2015-02-19 22:49:48 -0500

    Initial, example v2 guild APIs

---
+++ v2/guild/guild.js
@@ -0,0 +1,19 @@
+// GET /v2/guild?guild_id=75FD83CF-0C45-4834-BC4C-097F93A487AF
+{
+    "guild_id": "75FD83CF-0C45-4834-BC4C-097F93A487AF",
+    "guild_name": "Veterans Of Lions Arch",
+    "tag": "LA",
+    "motd": "This is an example MOTD. Check out our website at gw2.com", // Unsure about this one... many guild put private, TS information in the motd, so this shouldn't be publicly available
+    "home_world": 1007,
+    "member_count": 24,
+    "influence": 1036381,
+    "merits": 30,
+    "emblem": {
+        "background_id": 27,
+        "foreground_id": 114,
+        "flags": [ ],
+        "background_color_id": 11,
+        "foreground_primary_color_id": 584,
+        "foreground_secondary_color_id": 64
+    }
+}
