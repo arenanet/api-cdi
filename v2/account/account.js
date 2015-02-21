5b3f4fb4e1ce644656db5c7a044a43a4c52d58e2
lye <lye@>
2015-02-21 14:26:11 -0800

    Split bank/materials out from /v2/account and into separate endpoints.
     * Un-sparseify the bank array to indicate the total storage available
       and make it a bit cleaner to display via a UI.
     * Put the "type" field on material storage objects to indicate
       which category they belong to.
     * Also make the material storage objects non-sparse to indicate all
       available materials within a category.

--- v2/account/account.js
+++ v2/account/account.js
@@ -1,45 +1,68 @@
 // GET /v2/account
 // Authorization: Bearer oauth2-token
-
-// With the "account" scope:
+// Requires "account" scope.
 {
 	"id"    : "account-guid",
 	"name"  : "Lawton.1234",
 	"world" : 1007
 }
 
-// With the "account" and "inventories" scopes:
-{
-	"id"        : "account-guid",
-	"name"      : "Lawton.1234",
-	"world"     : 1007
-	"bank"      : [
-		{
-			"id"       : 19699,
-			"slot"     : 0,
-			"count"    : 250
-		},
-		{
-			"id"       : 19699,
-			"slot"     : 1,
-			"count"    : 12 
-		},
-		{
-			"id"       : 30692,
-			"slot"     : 4,
-			"count"    : 1,
-			"upgrades" : [ 24607 ]
-		}
-	],
-	"materials" : [
-		{
-			"id"       : 19699,
-			"count"    : 250
-		}
-	]
-}
+// GET /v2/account/bank
+// Authorization: Bearer oauth2-token
+// Requires "account" and "inventories" scopes.
+[
+	{
+		"id"       : 19699,
+		"count"    : 250
+	},
+	{
+		"count"    : 0
+	},
+	{
+		"id"       : 19699,
+		"count"    : 12 
+	},
+	{
+		"id"       : 30692,
+		"count"    : 1,
+		"upgrades" : [ 24607 ]
+	}
+]
+// Slot is indicated by array index; the number of slots is
+// implied by the length of the array. Empty slots are shown
+// with count=0 and id=undefined.
 
-// Note that the "bank" array is sparse -- any empty
-// slots are simply omitted from the result set. Slots
-// are 0-indexed. The "materials" objects do not include
-// slots as they cannot be re-ordered.
+// GET /v2/account/materials
+// Authorization: Bearer oauth2-token
+// Requires "account" and "inventories" scopes.
+[
+	{
+		"id" : 19699,
+		"type" : "common",
+		"count" : 250
+	},
+	{
+		"id" : 19670,
+		"type" : "common",
+		"count" : 0
+	},
+	{
+		"id" : 234,
+		"type" : "fine",
+		"count" : 3
+	}
+]
+// Where "type" is one of:
+//
+//  * "common"
+//  * "fine"
+//  * "rare"
+//  * "ascended"
+//  * "jewels"
+//  * "cooking"
+//  * "festive"
+//
+// And indicates the category the material is stored in.
+//
+// Array index indicates the slot; empty slots are indicated 
+// with count=0.
