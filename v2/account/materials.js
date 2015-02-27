9d90702d59db055fb522319aaf0b429e40fc22cf
lye <lye@>
2015-02-27 11:37:35 -0800

    Make the /v2/materials changes proposed in the /v2/account PR.
    Basically changes the following:
    
     * Add an unauthenticated /v2/materials to pull the list of item ids that
       can be stored in the account bank (and what categories they belong to).
     * Make /v2/account/materials use array index for ordering rather than a
       `slot` field.
     * Add a reference between the /v2/account/materials response items that
       indicates what category they belong to.

--- v2/account/materials.js
+++ v2/account/materials.js
@@ -4,31 +4,26 @@
 [
 	{
 		"id" : 19699,
-		"type" : "common",
+		"category" : 5,
 		"count" : 250
 	},
 	{
 		"id" : 19670,
-		"type" : "common",
+		"category" : 5,
 		"count" : 0
 	},
 	{
 		"id" : 234,
-		"type" : "fine",
+		"category" : 4,
 		"count" : 3
-	}
+	},
+	...
 ]
-// Where "type" is one of:
-//
-//  * "common"
-//  * "fine"
-//  * "rare"
-//  * "ascended"
-//  * "jewels"
-//  * "cooking"
-//  * "festive"
-//
-// And indicates the category the material is stored in.
+// Where "category" refers to an id fetchable via /v2/materials.
+// Note that you can just grab all of the categories via
+// /v2/materials?ids=all (in the language of your choice) so
+// it's not a terrible number of round-trips.
 //
 // Array index indicates the slot; empty slots are indicated 
-// with count=0.
+// with count=0. They'll still have the "id" to indicate which
+// item id can be stored in them.
