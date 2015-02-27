c0aa094cebd52d1c9ce5040825be900e011ec3d9
lye <lye@>
2015-02-27 11:25:18 -0800

    Add in /v2/account/bank; split off from the /v2/account PR.

---
+++ v2/account/bank.js
@@ -0,0 +1,25 @@
+// GET /v2/account/bank
+// Authorization: Bearer oauth2-token
+// Requires "account" and "inventories" scopes.
+[
+	{
+		"id"       : 19699,
+		"count"    : 250
+	},
+	{
+		"count"    : 0
+	},
+	{
+		"id"       : 19699,
+		"count"    : 12 
+	},
+	{
+		"id"       : 30692,
+		"count"    : 1,
+		"upgrades" : [ 24607 ]
+	}
+]
+// Slot is indicated by array index; the number of slots is
+// implied by the length of the array. Empty slots are shown
+// with count=0 and id=undefined.
+
