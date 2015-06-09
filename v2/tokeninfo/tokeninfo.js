4e86d5519d4a3ceb06d9b06a6ac3c5bf802c4fe1
darthmaim <darth.maim@arcor.de>
2015-06-09 23:46:15 +0200

    Added "id" to /v2/tokeninfo

--- v2/tokeninfo/tokeninfo.js
+++ v2/tokeninfo/tokeninfo.js
@@ -1,6 +1,7 @@
 // GET /v2/tokeninfo
 // Authorization: Bearer api-key
 {
+	"id": "GUID-OF-THIS-TOKEN",
 	"name": "API key for super awesome app",
 	"permissions": ["account", "characters", "guilds"]
 }
