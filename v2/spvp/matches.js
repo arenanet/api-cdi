de9e94e0633cab5c86ad9d703bfe418f5fe67812
Red <chris_veenman@hotmail.com>
2015-06-20 19:23:29 +0200

    SPvP Matches Proposal

--- v2/spvp/matches.js
+++ v2/spvp/matches.js
@@ -1,129 +1,59 @@
-// GET /v2/commerce/transactions
-// Authorization: Bearer access-token
-// NOTE: requires 'account' and 'tradingpost' scopes.
+// GET /v2/spvp/matches
+// Authorization: GW2 Token
+// Dependencies: 
+//	- /v2/character
 
-[
-	"current",
-	"history"
-]
-
-// GET /v2/commerce/transactions/current
-// GET /v2/commerce/transactions/history
-
-[
-	"buys",
-	"sells"
-]
-
-// GET /v2/commerce/transactions/current/buys
-// GET /v2/commerce/transactions/current/sells
+// Retrieves the last played ranked/unranked matches of the character
+// GET /v2/spvp/matches?character=Field Strategist
 
 [
 	{
-		id: 1999,
-		item_id: 19699,
-		price: 1004,
-		quantity: 20,
-		created: "2014-12-15T14:43:36+00:00"
+		matchid: 1234,
+		match_start: "2014-12-15T14:43:35+00:00",
+		match_end: "2014-12-15T14:43:35+00:00",
+		ranked: true,
+		blue_score: 500,
+		red_score: 350,
+		blue:
+		{
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>
+		},
+		red:
+		{
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>
+		}
+		
 	},
 	{
-		id: 1997,
-		item_id: 19699,
-		price: 1003,
-		quantity: 20,
-		created: "2014-12-15T14:43:35+00:00"
-	},
-	...
-]
-
-// GET /v2/commerce/transactions/history/buys
-// GET /v2/commerce/transactions/history/sells
-
-[
-	{
-		id: 1000,
-		item_id: 19699,
-		price: 1004,
-		quantity: 20,
-		created: "2014-12-15T14:59:38+00:00",
-		purchased: "2014-12-15T14:59:39+00:00"
-	},
-	{
-		id: 999,
-		item_id: 19699,
-		price: 1003,
-		quantity: 20,
-		created: "2014-12-15T14:59:36+00:00",
-		purchased: "2014-12-15T14:59:37+00:00"
-	}
-	...
-]
-
-
-// Example transaction flow: buying an item.
-// Initial state: no pending orders:
-// GET /v2/commerce/transactions/current/buys
-[]
-
-// GET /v2/commerce/transactions/history/buys
-[]
-
-// List a buy order for something.
-// GET /v2/commerce/transactions/current/buys
-[
-	{
-		id : 42,
-		item_id : 12138,
-		price : 1002,
-		quantity: 100,
-		created: "2014-12-15T14:59:36+00:00"
-	}
-]
-
-// Someone sells you some butter.
-// GET /v2/commerce/transactions/current/buys
-[
-	{
-		id : 42,
-		item_id : 12138,
-		price : 1002,
-		quantity: 92,
-		created: "2014-12-15T14:59:36+00:00"
-	}
-]
-
-// GET /v2/commerce/transactions/history/buys
-[
-	{
-		id : 103,
-		item_id : 12138,
-		price : 1002,
-		quantity: 8,
-		created: "2014-12-15T14:59:36+00:00"
-		purchased: "2014-12-15T15:59:36+00:00"
-	}
-]
-
-// Then someone sells you the rest.
-// GET /v2/commerce/transactions/current/buys
-[]
-
-// GET /v2/commerce/transactions/history/buys
-[
-	{
-		id : 112,
-		item_id : 12138,
-		price : 1002,
-		quantity: 92,
-		created: "2014-12-15T14:59:36+00:00"
-		purchased: "2014-12-15T15:05:00+00:00"
-	},
-	{
-		id : 103,
-		item_id : 12138,
-		price : 1002,
-		quantity: 8,
-		created: "2014-12-15T14:59:36+00:00"
-		purchased: "2014-12-15T15:59:36+00:00"
+		matchid: 4321,
+		match_start: "2014-12-15T14:43:35+00:00",
+		match_end: "2014-12-15T14:43:35+00:00",
+		ranked: false,
+		blue_score: 321,
+		red_score: 500,
+		blue:
+		{
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>
+		},
+		red:
+		{
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>,
+			<Charactername>
+		}	
 	}
 ]
