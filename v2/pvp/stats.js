7ef15be404b18673e999ac04bd05eb7383b05403
lye <lye@>
2015-06-22 13:12:48 -0700

    Initial pass at /v2/pvp/stats, which provides aggregate match data.

---
+++ v2/pvp/stats.js
@@ -0,0 +1,46 @@
+// GET /v2/pvp/stats?access_token=token
+// Scopes: account, pvp
+
+[
+	{
+		aggregate : {
+			wins       : 1,
+			losses     : 2,
+			desertions : 0,
+			byes       : 2,
+			forfeits   : 0
+		},
+		professions : {
+			mesmer : {
+				wins       : 0,
+				losses     : 1,
+				desertions : 0,
+				byes       : 1,
+				forfeits   : 0
+			},
+			thief: {
+				wins       : 1,
+				losses     : 1,
+				desertions : 0,
+				byes       : 1,
+				forfeits   : 0
+			}
+		},
+		ladders : {
+			unranked : {
+				wins       : 0,
+				losses     : 1,
+				desertions : 0,
+				byes       : 0,
+				forfeits   : 0
+			},
+			ranked : {
+				wins       : 2,
+				losses     : 1,
+				desertions : 0,
+				byes       : 2,
+				forfeits   : 0
+			}
+		}
+	}
+]
