12a670e8550f43733f75678b0a2dcb9bb3731d3b
Markus <markus@gw2-tools.com>
2015-07-07 11:29:01 +0200

    Update account.js

--- v2/account/account.js
+++ v2/account/account.js
@@ -5,8 +5,6 @@
 	"id"     : "account-guid",
 	"name"   : "Lawton.1234",
 	"world"  : 1007,
-	"commander": "yes or no",
-	"fractallevel": 11,
 	"guilds" : [
 		"DA9137CD-3A86-E411-B57A-00224D566B58",
 		"1F5F70AA-1DB6-E411-A2C4-00224D566B58",
