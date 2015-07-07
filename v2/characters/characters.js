8eca1a493d5df5e70308cc75928ec2d39d62af3c
Markus <markus@gw2-tools.com>
2015-07-07 12:08:50 +0200

    Update characters.js

--- v2/characters/characters.js
+++ v2/characters/characters.js
@@ -11,23 +11,6 @@
 	race: "Human",
 	gender: "Female",
 	profession: "Thief",
-	crafting:[
-		"Leatherworker",
-		"Weaponsmith",
-		"Tailor",
-		null,
-		],
-	order:"Order of Whispers",
-	storymode:[
-		"Ascalonian Catacombs",
-		"Caudecus's Manor",
-		"Twilight Arbor",
-		null,
-		null,
-		null,
-		null,
-		"The Ruined City of Arah",
-		],
 	level: 80,
 	guild: "1F5F70AA-1DB6-E411-A2C4-00224D566B58"
 }
@@ -41,23 +24,6 @@
 		race: "Human",
 		gender: "Female",
 		profession: "Thief",
-			crafting:[
-			"Leatherworker",
-			"Weaponsmith",
-			"Tailor",
-			null,
-			],
-		order:"Order of Whispers",
-		storymode:[
-			"Ascalonian Catacombs",
-			"Caudecus's Manor",
-			"Twilight Arbor",
-			null,
-			null,
-			null,
-			null,
-			"The Ruined City of Arah",
-			],
 		level: 80,
 		guild: "1F5F70AA-1DB6-E411-A2C4-00224D566B58"
 	}
@@ -71,23 +37,6 @@
 	race: "Human",
 	gender: "Female",
 	profession: "Thief",
-	crafting:[
-		"Leatherworker",
-		"Weaponsmith",
-		"Tailor",
-		null,
-		],
-	order:"Order of Whispers",
-	storymode:[
-		"Ascalonian Catacombs",
-		"Caudecus's Manor",
-		"Twilight Arbor",
-		null,
-		null,
-		null,
-		null,
-		"The Ruined City of Arah",
-		],
 	level: 80,
 	guild: "1F5F70AA-1DB6-E411-A2C4-00224D566B58",
 	equipment: {
