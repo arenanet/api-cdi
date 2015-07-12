0d5769bdc469ada703b8bafae380f65cd627eb27
smiley <smiley@chillerlan.net>
2015-07-12 22:43:41 +0200

    added v2/floors/events

---
+++ v2/floors/events-location.js
@@ -0,0 +1,27 @@
+[
+	// an events.location object may represent one of the following
+	{
+		"type": "circle",
+		"coords": [12058, 13661],
+		"radius": 250
+	},
+	{
+		"type": "polyline",
+		"start": [12418, 12867],
+		"coords": [
+			[12418, 12867],
+			[12410, 12900],
+			[12410, 12934]
+		]
+	},
+	{
+		"type": "polygon",
+		"coords": [
+			[11358, 13107],
+			[11270, 13233],
+			[11288, 13367],
+			[11424, 13407],
+			[11588, 13477]
+		]
+	}
+]
