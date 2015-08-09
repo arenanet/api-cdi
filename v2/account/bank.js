9ee41ba1f7f1589dfd3e980b190439c220d67023
bobbyjoemoma <bobbyjoemoma@users.noreply.github.com>
2015-08-09 19:10:40 -0400

    added currency to response
    Accessing the Bank endpoint will provide the amount of deposited money in the bank as well as the previously returned array of items in the banks storage.

--- v2/account/bank.js
+++ v2/account/bank.js
@@ -1,71 +1,75 @@
 // GET /v2/account/bank
 // Authorization: Bearer oauth2-token
 // Requires "account" and "inventories" scopes.
-[
-	null,
-	{
-		id: 46774,
-		slot: 1,
-		count: 1,
-		upgrades: [
-			24675
-		]
-	},
-	{
-		id: 48087,
-		slot: 2,
-		count: 1,
-		skin: 89
-	},
-	null,
-	{
-		id: 46760,
-		slot: 4,
-		count: 1,
-		skin: 3706,
-		upgrades: [
-			24661
-		]
-	},
-	null,
-	null,
-	null,
-	null,
-	null,
-	{
-		id: 19996,
-		slot: 10,
-		count: 1
-		infusions: [
-			1234,
-			5678
-		]
-	},
-	{
-		id: 19976,
-		slot: 11,
-		count: 4
-	},
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	null
-]
-// Slot is indicated by array index; the number of slots is
-// implied by the length of the array. Empty slots are indicated
-// by null values.
+{
+	currency: 98877,  // 9g88s77c
+	items: [
+		null,
+		{
+			id: 46774,
+			slot: 1,
+			count: 1,
+			upgrades: [
+				24675
+			]
+		},
+		{
+			id: 48087,
+			slot: 2,
+			count: 1,
+			skin: 89
+		},
+		null,
+		{
+			id: 46760,
+			slot: 4,
+			count: 1,
+			skin: 3706,
+			upgrades: [
+				24661
+			]
+		},
+		null,
+		null,
+		null,
+		null,
+		null,
+		{
+			id: 19996,
+			slot: 10,
+			count: 1
+			infusions: [
+				1234,
+				5678
+			]
+		},
+		{
+			id: 19976,
+			slot: 11,
+			count: 4
+		},
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null
+	]
+	// Slot is indicated by array index; the number of slots is
+	// implied by the length of the array. Empty slots are indicated
+	// by null values.
+}
+
 
