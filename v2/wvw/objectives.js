37c7c18756a71d93c2bc612cd11238a573d02877
lye <lye@>
2015-09-11 18:50:01 -0700

    Add sector_ids to /v2/wvw/objectives.

--- v2/wvw/objectives.js
+++ v2/wvw/objectives.js
@@ -6,12 +6,13 @@
 
 // GET /v2/wvw/objectives?id=95-43
 {
-	id       : "95-43",
-	type     : "Camp",
-	map_type : "GreenHome",
-	map_id   : 95,
-	name     : "Hero's Lodge",
-	coord    : [
+	id        : "95-43",
+	type      : "Camp",
+	map_type  : "GreenHome",
+	map_id    : 95,
+	sector_id : 349,
+	name      : "Hero's Lodge",
+	coord     : [
 		6931.58,
 		14502.1
 	]
@@ -20,35 +21,38 @@
 // GET /v2/wvw/objectives?page=0&page_size=3
 [
 	{
-		id : "968-85",
-		type : "Resource",
-		map_type : "EdgeOfTheMists",
-		map_id   : 968,
-		name     : "Tytone Perch Reactor",
-		coord    : [
+		id        : "968-85",
+		type      : "Resource",
+		map_type  : "EdgeOfTheMists",
+		map_id    : 968,
+		sector_id : 495,
+		name      : "Tytone Perch Reactor",
+		coord     : [
 			7852.89,
 			9855.56
 		],
 		marker   : "https://render.guildwars2.com/..."
 	},
 	{
-		id       : "94-32",
-		type     : "Keep",
-		map_type : "RedHome",
-		map_id   : 94,
-		name     : "Etheron Hills",
-		coord    : [
+		id        : "94-32",
+		type      : "Keep",
+		map_type  : "RedHome",
+		map_id    : 94,
+		sector_id : 956,
+		name      : "Etheron Hills",
+		coord     : [
 			11212.6,
 			10929.3
 		]
 	},
 	{
-		id       : "38-11",
-		type     : "Tower",
-		map_type : "Center",
-		map_id   : 38,
-		name     : "Aldon's Ledge",
-		coord    : [
+		id        : "38-11",
+		type      : "Tower",
+		map_type  : "Center",
+		map_id    : 38,
+		sector_id : 485,
+		name      : "Aldon's Ledge",
+		coord     : [
 			9417.39,
 			14790.7
 		]
@@ -76,7 +80,8 @@
 //   * ObsidianSanctum
 //   * EdgeOfTheMists
 //
-// coord may not be present on all objects.
+// coord and sector_id may not be present on all objects. When one is
+// missing the other will be too -- the coord is the sector centroid.
 //
 // marker may not be present on all objects.
 //
