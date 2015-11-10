7e4f4759b6118375911a8aa619f875a32969806d
Mattia <mattia.belletti@gmail.com>
2015-11-10 11:12:06 +0100

    Known recipes
    A proposal to get the list of known recipes for each character.
    
    This is particularly useful for various reasons:
    
    - crafting helpers know which recipes need to be bought or not
    
    - find which character has learned a recipe without logging in and out
    of various crafters

---
+++ v2/characters/recipes.js
@@ -0,0 +1,17 @@
+// GET /v2/characters/Hello/recipes
+// Authorization: Bearer oauth2-token
+// Requires "account" and "characters" scope.
+[
+	{
+		"discipline": "Leatherworker",
+		"recipes": [434, 543, 414]
+	},
+	{
+		"discipline": "Armorsmith",
+		"recipes": [656, 367, 842, 483, 18]
+	},
+	{
+		"discipline": "Jeweler",
+		"recipes": [435, 678, 989, 113]
+	}
+]
