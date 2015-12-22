99388aaecdba8b78bcd6b5f3a91df917ea8b9696
lye <lye@>
2015-12-22 10:45:56 -0800

    Add /v2/guild/:id/treasury which dumps treasury contents.

---
+++ v2/guild/treasury.js
@@ -0,0 +1,18 @@
+// Authenticated endpoint that outputs guild treasury.
+// **only available with keys from guild leaders for now**
+// Requires scope: "guilds"
+
+// GET /v2/guilds/:id/treasury?access_token=:token
+
+[
+	{
+		id    : 12138,
+		count : 200
+	},
+	{
+		id    : 19684,
+		count : 3493
+	}
+]
+
+// "id" is an item id and can be resolved against /v2/items.
