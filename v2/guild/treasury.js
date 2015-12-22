fd07b63de3805ebd8771a59fe87484a04746509c
lye <lye@>
2015-12-22 12:14:28 -0800

    Add in "needed_by" upgrade details in /v2/guild/:id/treasury.
    This should save some back/forth for applications displaying the current
    treasury status. Decided to omit the "max" field discussed in the PR since
    it was literally just a reduce away.

--- v2/guild/treasury.js
+++ v2/guild/treasury.js
@@ -6,13 +6,33 @@
 
 [
 	{
-		id    : 12138,
-		count : 200
+		id        : 12138,
+		count     : 200,
+		needed_by : [
+			{
+				upgrade_id : 42,
+				count      : 1000
+			}
+		]
 	},
 	{
-		id    : 19684,
-		count : 3493
+		id        : 19684,
+		count     : 3493,
+		needed_by : [
+			{
+				upgrade_id : 42,
+				count      : 3000
+			},
+			{
+				upgrade_id : 43,
+				count      : 3000
+			}
+		]
 	}
 ]
 
 // "id" is an item id and can be resolved against /v2/items.
+
+// "needed_by" is the list of in-progress upgrades that require
+// this item. The maximum count of an item can be computed by
+// summing the "needed_by[n].count" values.
