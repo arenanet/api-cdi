779ca44e9bd54f9e1facf802cf22b4b24998b8bb
lye <lye@>
2016-01-18 14:56:01 -0800

    Add /v2/guild/:id/stash.

---
+++ v2/guild/stash.js
@@ -0,0 +1,25 @@
+// Authenticated endpoint that outputs guild stash contents.
+// **only available with keys from guild leaders for now**
+// Requires scope: "guilds"
+
+[
+	{
+		"upgrade_id" : 58,
+		"size" : 100,
+		"coins" : 100039,
+		"inventory" : [
+			null,
+			null,
+			{
+				"id" : 12138,
+				"count" : 250
+			},
+			null,
+			null,
+			null,
+			// ... 100 entries in total.
+		]
+	},
+	// ...
+]
+
