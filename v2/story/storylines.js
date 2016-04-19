2765ee76d0f3d3800f73501e24fdb51f3b62fa4e
Andreas Girgensohn <andreasg123@gmail.com>
2016-04-19 12:09:20 -0700

    First version of story progression

---
+++ v2/story/storylines.js
@@ -0,0 +1,26 @@
+// GET /v2/story/storylines
+
+[ 1, 2, 3, 4 ]
+
+// GET /v2/story/storylines/1
+// GET /v2/story/storylines?id=1
+
+{
+	"id": 1,
+	"name": "Personal Story"
+}
+
+// GET /v2/story/storylines?ids=1,3
+
+[
+	{
+		"id": 1,
+		"name": "Personal Story",
+		"parts": [1011, 1111, 1112, 1211]
+	},
+	{
+		"id" 3,
+		"name": "Living World season 2"
+		"parts": [3100, 3200]
+	}
+]
