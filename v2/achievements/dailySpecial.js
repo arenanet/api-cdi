8bf4e5521bad88a2c52fd30d5a20bdafa0c45b72
darthmaim <git@darthmaim.de>
2017-01-22 11:45:41 +0100

    Add /v2/achievements/daily/special

---
+++ v2/achievements/dailySpecial.js
@@ -0,0 +1,18 @@
+// Provides information about current special events.
+
+// GET /v2/achievements/daily/special
+
+{
+    name  : "Lunar New Year",
+    start : "2017-01-17T00:00:00.000Z",
+    end   : "2017-01-31T07:00:00.000Z",
+    rewards: [
+        {
+            "type"  : "Item",
+            "id"    : 68642,
+            "count" : 1
+        }
+    ]
+}
+
+// If there's no temporary content, {} is returned.
