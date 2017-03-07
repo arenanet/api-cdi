167212c13d036c2d0a74bb08657d37b5c58a3839
lye <lye@>
2017-03-07 15:53:00 -0800

    Add /v2/gliders and /v2/account/gliders.

---
+++ v2/account/gliders.js
@@ -0,0 +1,7 @@
+// Authenticated endpoint that returns glider unlocks. Requires
+// "account" and "unlocks" scopes. Ids can be resolved against
+// /v2/gliders.
+
+// GET /v2/account/gliders?access_token=foo
+
+[ 1, 2, 3 ]
