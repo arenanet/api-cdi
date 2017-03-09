15ae62725dfdc81b959b4fee27a62eef8061f64b
lye <lye@>
2017-03-09 14:39:58 -0800

    Add /v2/pvp/heroes and /v2/account/pvp-heroes.

---
+++ v2/account/pvp-heroes.js
@@ -0,0 +1,11 @@
+// Authenticated endpoint; provides unlocked PvP hero skins.
+// Requires the "account" and "unlocks" scopes.
+
+// GET /v2/account/pvp-heroes?access_token=foo
+
+[ 1, 2, 3 ]
+
+// NOTE:
+//  * ids refer to the skin ids of /v2/pvp/heroes. Recommend using
+//    ?ids=all on that endpoint to resolve them back to the appropriate
+//    data.
