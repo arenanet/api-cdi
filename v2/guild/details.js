544db504bc1b0b1c324e17c4b2a4baa92b809add
lye <unpantsu@gmail.com>
2017-03-21 23:31:24 -0700

    /v2/guild/:id - remove incorrect docs.
    Guess this was from a PR that I merged without reading closely enough. There's some additional bits that aren't documented here -- specifically that this endpoint provides both unauthenticated and authenticated access (providing a functional API key of a guild member will let you see some extra data, like MOTD). Will do that tomorrow.

--- v2/guild/details.js
+++ v2/guild/details.js
@@ -1,19 +1,6 @@
 // Unauthenticated endpoint which outputs public details of a guild.
 
-// GET /v2/guilds
-// This will output all currently registered guilds ids to the user.
-// Example:
-[
-    "F05AA71F-37CD-4857-B632-517909765E4F",
-    "20034A78-E176-44B5-A5FE-07F80C1E7A05",
-    "3E579C5D-055F-48A6-9E09-F921588F83B1",
-    "61443D40-BACD-4D09-83A6-3C761A7BAAE3",
-    ...
-]
-
-// GET v2/guilds/:id or v2/guilds?ids=:id
-// This will output the public details of a guild a given od
-// Example:
+// GET v2/guilds/:id
 {
    "id": "75FD83CF-0C45-4834-BC4C-097F93A487AF",
    "name": "Veterans Of Lions Arch",
@@ -27,6 +14,3 @@
      "foreground_secondary_color": 64
    }
  }
-
- // GET v2/guilds?ids=:ids
- // Same as above only that an array of guild details is returned
