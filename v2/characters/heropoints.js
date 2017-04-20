a83f25dcf819b9886ec04a65e4ed23bbb6a0066a
Archomeda <archomeda@hotmail.com>
2017-04-20 04:41:50 +0200

    Add /v2/characters/:id/heropoints and hero point ids to /v2/continents

---
+++ v2/characters/heropoints.js
@@ -0,0 +1,11 @@
+// GET /v2/characters/Hello/heropoints
+// Authorization: Bearer oauth2-token
+// Requires "account", "characters", "progression" permissions.
+
+[
+	"0-0",
+	"0-35",
+	"0-41",
+	"0-64",
+	"0-121"
+]
